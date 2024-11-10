8761559cb744d42dc820674d4e97d64306167d04
Pavel Shirshov <pshirshov@eml.cc>
2024-11-10 17:46:22 +0000

    wip

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nodered-utilities
+
+```bash
+cd /data/home/nodered/.node-red/ && sudo -u nodered -g nodered npm update nodered-utilities && killall node-red
+```
--- index.js
+++ index.js
@@ -15,5 +15,47 @@ function clamp_positive(value) {
     return value;
 }
 
+
+class TariffBand {
+    static Day = new TariffBand('Day');
+    static Night = new TariffBand('Night');
+    static NightExtended = new TariffBand('NightExtended');
+    static Boost = new TariffBand('Boost');
+
+    constructor(name) {
+        this.name = name;
+    }
+    toString() {
+        return `TariffBand.${this.name}`;
+    }
+}
+
+function tariff_band(now) {
+    const isDayTime = now.getHours() < 23 && now.getHours() >= 8;
+    if (isDayTime) {
+        return TariffBand.Day;
+    }
+
+    const isBoostTime = now.getHours() >= 2 && now.getHours() < 5;
+    if (isBoostTime) {
+        return TariffBand.Boost;
+    }
+
+    const isNightTime = now.getHours() >= 23 || now.getHours() < 2;
+    if (isNightTime) {
+        return TariffBand.Night;
+    }
+
+    const isExtendedChargeTime = now.getHours() >= 5 && now.getHours() < 8;
+    if (isExtendedChargeTime) {
+        return TariffBand.NightExtended;
+    }
+
+    throw `tariff_band failed to map ${now}`;
+}
+
 module.exports.clamp = clamp;
 module.exports.clamp_positive = clamp_positive;
+
+module.exports.tariff_band = tariff_band;
+module.exports.TariffBand = TariffBand;
